620fa9851b9eee10b4475572b7f40c2ae69b7edd
Brittani Savage <brittanisavage@gmail.com>
2021-04-13 11:32:10 -0500

    Tweaked learn page to add info

--- Re-Chi-Cyle/src/components/find.vue
+++ Re-Chi-Cyle/src/components/find.vue
@@ -5,9 +5,9 @@
     <div class='location-container'>
     <div class='recycling-location'>
       <div>
-      <iframe width="600" height="350" style="border:0; margin: 10px;" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJAYCP3L7RD4gRZhZzBLZuWGc&key=AIzaSyCS7mPVkpU1BHc5PEprtC9-g0zSRrWUIN4"></iframe><br/><h3>Residential Recycling Drop-Off Center</h3></div>
+      <iframe width="600" height="350" style="border:0; margin: 10px;" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJAYCP3L7RD4gRZhZzBLZuWGc&key=AIzaSyCS7mPVkpU1BHc5PEprtC9-g0zSRrWUIN4"></iframe><br/><h3>Residential Recycling Drop-Off Center</h3><h4>Note: Accepts all Blue Bin materials</h4></div>
       <div>
-      <iframe width="600" height="350" style="border:0; margin: 10px;" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJcYNJ54gsDogR5DoKnOYEJ8M&key=AIzaSyCS7mPVkpU1BHc5PEprtC9-g0zSRrWUIN4"></iframe><br/><h3>Residential Recycling Drop-Off Center</h3></div>
+      <iframe width="600" height="350" style="border:0; margin: 10px;" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJcYNJ54gsDogR5DoKnOYEJ8M&key=AIzaSyCS7mPVkpU1BHc5PEprtC9-g0zSRrWUIN4"></iframe><br/><h3>Residential Recycling Drop-Off Center</h3><h4>Note: Accepts all Blue Bin materials</h4></div>
       <div>
       <iframe width="600" height="350" style="border:0; margin: 10px;" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJjTO8BSnTD4gRgS8Y_8ZX1C0&key=AIzaSyCS7mPVkpU1BHc5PEprtC9-g0zSRrWUIN4"></iframe><br/><h3>Household Chemicals and Computer Recycling Facility</h3><h4>Note: A full list of accepted and rejected materials are available <a href='https://www.chicago.gov/city/en/depts/cdph/supp_info/inspections---permitting/household_chemicalscomputerrecyclingfacilityaccepteditems.html'>here</a></h4></div>
       <div>
--- Re-Chi-Cyle/src/components/learn.vue
+++ Re-Chi-Cyle/src/components/learn.vue
@@ -42,17 +42,17 @@
         <img class='broken-bottle-icon' alt='broken bottle' src='../assets/icons8-broken-bottle-100.png'/>
         <img class='light-bulb-icon' alt='spiral light bulb' src='../assets/icons8-spiral-bulb-100.png'/>
         <img class='broken-glass-icon' alt='cracked wine glass' src='../assets/icons8-fragile-100-2.png'/>
-        <br/>Broken Glass, Drinking Glasses, and Lightbulbs
+        <br/>Broken Glass, Drinking Glasses, and Lightbulbs*
         </div><br/>
     <div class='plastic-bags-box'>
         <img class='pill-icon' alt='pill bottle' src='../assets/icons8-pill-bottle-100-2.png'/>
         <img class='plastic-bag-icon' alt='plastic bag' src='../assets/bag.png'/><img class='styrofoam-icon' alt='styrofoam' src='../assets/icons8-plastic-food-container-100.png'/>
-        <br/>Pill Bottles, Plastic Bags, and Styrofoam
+        <br/>Pill Bottles****, Plastic Bags***, and Styrofoam**
         </div><br/>
     <div class='electronic-box'>
         <img class='pc-icon' alt='broken computer' src='../assets/icons8-broken-computer-100.png'/>
         <img class='battery-icon' alt='battery' src='../assets/battery.png'/>
-        <img class='phone-icon' alt='broken phone' src='../assets/icons8-broken-phone-100.png'/><br/>Electronics and Batteries
+        <img class='phone-icon' alt='broken phone' src='../assets/icons8-broken-phone-100.png'/><br/>Electronics and Batteries*
         </div><br/>
     </div>
     <div class='neither-container'>
@@ -62,12 +62,14 @@
         <img class='learn-icon' alt='maple leaf' src='../assets/icons8-maple-leaf-100.png'/>
         <img class='learn-icon' alt='grass' src='../assets/icons8-grass-100.png'/><br/>
         Yard Waste
+        <h5>Must be bagged</h5>
         </div>
     <div class='furn-box'>
         <img class='learn-icon' alt='couch' src='../assets/icons8-three-seater-sofa-96.png'/><br/>Furniture
         </div>
     <div class='bigapp-box'>
         <img class='learn-icon' alt='fridge' src='../assets/icons8-fridge-100.png'/><br/>Large Appliances
+        <h5>Appliance must be broken - doors must be removed</h5>
         </div>
     <div class='mattress-box'>
         <img class='mattress-icon' alt='mattress' src='../assets/mattress.png'/><br/>Mattresses
@@ -75,6 +77,8 @@
     </div><br/>
     </div>
 </div>
+<h5 style='font-size: 14px; width: 50%; margin: 0 auto; margin-top: 35px; line-height: 200%;'>*Batteries, lightbulbs, most electronics, and most chemicals can be recycled at the Household Chemicals and Computer Recycling Facility - major electronic stores also recycle batteries<br/> **Styrofoam can be recycled at the Dart Container Facility<br/> ***Plastic bags can be recycled at most local grocery stores<br/> ****Pill bottles can be recycled at most local pharmacies
+</h5>
 </div>
 </template>
 
@@ -137,7 +141,7 @@
     margin-left: 7px;
 }
 
-.cat-food-icon, .apple-icon, .banana-icon, .plastic-bag-icon, .battery-icon, .steak-icon{
+.cat-food-icon, .apple-icon, .banana-icon, .plastic-bag-icon, .battery-icon, .steak-icon, .learn-icon{
     width: 100px;
     height: 100px;
 }
